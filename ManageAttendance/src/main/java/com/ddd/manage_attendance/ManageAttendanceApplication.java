2c809c95e11286c3e984869def0afb6085168295
JSL107 <juneseok0107@naver.com>
2025-11-29 19:52:23 +0900

    init project

---
+++ ManageAttendance/src/main/java/com/ddd/manage_attendance/ManageAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.ddd.manage_attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageAttendanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageAttendanceApplication.class, args);
+    }
+
+}
