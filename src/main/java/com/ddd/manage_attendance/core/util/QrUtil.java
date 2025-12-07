5455da15913b9e96552ddfca2e0eca25baff1577
JSL107 <juneseok0107@naver.com>
2025-12-07 21:18:21 +0900

    git rm -r --cahced.

--- src/main/java/com/ddd/manage_attendance/core/util/QrUtil.java
+++
@@ -1,9 +0,0 @@
-package com.ddd.manage_attendance.core.util;
-
-import java.util.Base64;
-
-public class QrUtil {
-    public static String toBase64(byte[] imageBytes) {
-        return Base64.getEncoder().encodeToString(imageBytes);
-    }
-}
--- src/main/java/com/ddd/manage_attendance/domain/auth/domain/UserRepository.java
+++
@@ -1,5 +0,0 @@
-package com.ddd.manage_attendance.domain.auth.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {}
