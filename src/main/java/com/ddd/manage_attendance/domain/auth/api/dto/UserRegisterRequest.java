24f9a9b6f467e75918d49ab29633025020eb2611
Jiwon-cho <gwon188@gmail.com>
2025-12-18 21:51:57 +0900

    refactor: 유저 저장 dto 직군 예시 변경

--- src/main/java/com/ddd/manage_attendance/domain/auth/api/dto/UserRegisterRequest.java
+++ src/main/java/com/ddd/manage_attendance/domain/auth/api/dto/UserRegisterRequest.java
@@ -10,7 +10,7 @@ public record UserRegisterRequest(
         @Schema(description = "이름", example = "홍길동") @NotBlank(message = "이름은 필수입니다.") String name,
         @Schema(description = "기수 ID", example = "1") @NotNull(message = "기수 ID는 필수입니다.")
                 Long generationId,
-        @Schema(description = "직군", example = "BE") @NotNull(message = "직군은 필수입니다.")
+        @Schema(description = "직군", example = "BACKEND") @NotNull(message = "직군은 필수입니다.")
                 JobRole jobRole,
         @Schema(description = "팀 ID", example = "1") @NotNull(message = "팀 ID는 필수입니다.")
                 Long teamId) {}
