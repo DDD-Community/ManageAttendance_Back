95598fcf81ed87bf6387e67763551a432f8d9124
Jiwon-cho <gwon188@gmail.com>
2025-12-28 02:44:41 +0900

    config: cors 이슈 수정

--- src/main/java/com/ddd/manage_attendance/core/config/swagger/SwaggerConfig.java
+++ src/main/java/com/ddd/manage_attendance/core/config/swagger/SwaggerConfig.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,7 +22,13 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI apiConfig() {
         return new OpenAPI()
-                .servers(List.of(new Server().url("https://api.dddstudy.site")))
                 .info(new Info().title("출석앱 API").description("DDD 출석앱 SWAGGER UI입니다."));
+    }
+
+    @Bean
+    public OpenApiCustomizer forceHttpsServerUrl() {
+        return openApi -> openApi.setServers(List.of(
+                new Server().url("https://api.dddstudy.site")
+        ));
     }
 }
